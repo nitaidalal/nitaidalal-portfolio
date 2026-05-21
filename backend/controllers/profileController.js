0634a6acc5ac01ebdb51b8ad9c2904be27d3da8c
nitaidalal <dalalnitai7@gmail.com>
2026-05-22 01:56:06 +0530

    Add profile controller and routes for managing user profiles, avatars, and resumes

---
+++ backend/controllers/profileController.js
@@ -0,0 +1,138 @@
+import Profile from "../models/profile.model.js";
+import {updateProfileSchema} from "../validators/profileValidator.js"
+import {successResponse,errorResponse} from "../utils/apiResponse.js"
+import { cloudinary } from "../config/cloudinary.js";
+import e from "express";
+
+// ─── @route  GET /api/profile ─────────────────────────
+// ─── @access Public ───────────────────────────────────
+export const getProfile = async(req,res,next) => {
+    try {
+        const profile = await Profile.findOne();
+
+        if(!profile){
+            return errorResponse(res,404,"Profile not found");
+        }
+        return successResponse(res,200,"Profile retrieved successfully",profile);
+    } catch (error) {
+        next(error);
+    }
+}
+
+// ─── @route  PUT /api/profile ─────────────────────────
+// ─── @access Private ──────────────────────────────────
+export const updateProfile = async(req,res,next) => {
+    try {
+        const result = updateProfileSchema.safeParse(req.body);
+        if (!result.success) {
+          const errors = result.error.errors.map((e) => e.message);
+          return errorResponse(res, 400, errors[0]);
+        }
+
+        const profile = await Profile.findOne();
+
+        if (!profile) {
+          return errorResponse(res, 404, "Profile not found");
+        }
+
+        //findOneAndUpdate - if no profile data exists, create one (upsert:true)
+        const profile = await Profile.findOneAndUpdate(
+            {},
+            { $set: result.data },
+            { new: true, upsert: true, runValidators: true }
+        );
+
+        return successResponse(res, 200, "Profile updated successfully", profile);
+    } catch (error) {
+        next(error);
+    }
+}
+
+// ─── @route  POST /api/profile/avatar ─────────────────────────
+// ─── @access Private ──────────────────────────────────
+export const updateAvatar = async(req,res,next) => {
+    try {
+        if(!req.file){
+            return errorResponse(res,400,"No file uploaded");
+        }
+
+        const profile = await Profile.findOne();
+
+        if(!profile){
+            return errorResponse(res,404,"Profile not found");
+        }
+
+        // If there's an existing avatar, delete it from Cloudinary
+        if(profile?.avatarPublicId){
+            try {
+                await cloudinary.uploader.destroy(
+                  profile.avatarPublicId
+                );
+              } catch (err) {
+                console.error(
+                  "Failed to delete old avatar:",
+                  err.message
+                );
+              }
+        }
+
+        const updatedprofile = await Profile.findOneAndUpdate(
+            {},
+            {
+                $set:{
+                    avatarUrl:req.file.path,
+                    avatarPublicId:req.file.filename,
+                },
+            },
+            {new:true, upsert:true}
+
+        )
+
+        return successResponse(res,200,"Avatar updated successfully", {avatarUrl:updatedprofile.avatarUrl});
+        
+    } catch (error) {
+        next(error);
+    }
+}
+
+// ─── @route  PUT /api/profile/resume ─────────────────
+// ─── @access Private ──────────────────────────────────
+export const updateResume = async(req,res,next) => {
+    try {
+        if(!req.file){
+            return errorResponse(res,400,"No pdf file uploaded");
+        }
+
+        const profile  = await Profile.findOne();
+        if(!profile){
+            return errorResponse(res,404,"Profile not found");
+        }
+
+        // If there's an existing resume, delete it from Cloudinary
+        if(profile?.resumePublicId){
+            try {
+                await cloudinary.uploader.destroy(profile.resumePublicId,
+                    { resource_type: "raw" } //PDF is treated as raw file in Cloudinary
+                );
+            } catch (error) {
+                console.error("Failed to delete old resume:", error.message);
+            }
+        }
+
+        const updatedProfile = await Profile.findOneAndUpdate(
+            {},
+            {
+                $set:{
+                    resumeUrl:req.file.path,
+                    resumePublicId:req.file.filename,
+                }
+            },
+            { new: true , upsert:true}
+        );
+
+        return successResponse(res,200,"Resume updated successfully",{resumeUrl:updatedProfile.resumeUrl});
+
+    } catch (error) {
+        next(error);
+    }
+}
